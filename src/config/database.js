e049155a74895a6375b6ddbd4047509191b8acea
ventascarlettpublicidad-cell <ventas.carlettpublicidad@gmail.com>
2026-02-25 10:36:33 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
