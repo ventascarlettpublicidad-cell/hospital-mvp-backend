cb98351da1e3687f135f29d956d295c93cc6e681
ventascarlettpublicidad-cell <ventas.carlettpublicidad@gmail.com>
2026-02-26 02:19:46 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(⁠ 🚀 Hospital Management API running on port ${PORT} ⁠);
+  console.log(⁠`Hospital Management API running on port ${PORT}`⁠);
 });
 
 // Ejecutar DB sin bloquear el servidor
