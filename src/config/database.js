2bb9ce573975ec4bd4efcc77c46893a3d029b212
ventascarlettpublicidad-cell <ventas.carlettpublicidad@gmail.com>
2026-02-26 01:14:37 -0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   ssl: {
     rejectUnauthorized: false
   }
